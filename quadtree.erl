91064fadabb79d76af224e75df6a65cab1c1a097
Fabian Linzberger <e@lefant.net>
2009-03-20 15:54:56 +0100

    quadtree neighbour function

--- quadtree.erl
+++ quadtree.erl
@@ -16,17 +16,19 @@
           x,
           y,
           size,
-          children,
-          status
+          children=undefined,
+          status=empty
 }).
 
 test() ->
     ?LOG("Debug is enabled"),
     dbg:tracer(),
     dbg:p(all, call),
-%%%     dbg:tpl(quadtree, intersects, 2, []),
-%%%     dbg:tpl(mtm, inner_prod, 4, []),
-%%%     dbg:tpl(mtm, receive_data, 3, []),
+%%%     dbg:tpl(quadtree, find_node, 2, []),
+%%%     dbg:tpl(quadtree, within_node, 2, []),
+%%%     dbg:tpl(quadtree, intersects_node, 2, []),
+%%%     dbg:tpl(quadtree, within, 2, []),
+%%%     dbg:tpl(quadtree, corners, 1, []),
 
     ListOfCircles = [
                      {-10,-5,3},
@@ -36,38 +38,40 @@ test() ->
     QuadTree = #node{
       x=0,
       y=0,
-      size=20,
-      children=undefined,
-      status=empty
+      size=20
      },
 
-    false = intersects(QuadTree,{100,100,1}),
-    false = intersects(QuadTree,{-100,-100,1}),
-    false = intersects(QuadTree,{100,-100,1}),
-    false = intersects(QuadTree,{-100,100,1}),
+    false = intersects_circle(QuadTree,{100,100,1}),
+    false = intersects_circle(QuadTree,{-100,-100,1}),
+    false = intersects_circle(QuadTree,{100,-100,1}),
+    false = intersects_circle(QuadTree,{-100,100,1}),
 
-    false = intersects(QuadTree,{100,100,85}),
-    false = intersects(QuadTree,{-100,100,85}),
-    false = intersects(QuadTree,{100,-100,85}),
-    false = intersects(QuadTree,{-100,-100,85}),
+    false = intersects_circle(QuadTree,{100,100,85}),
+    false = intersects_circle(QuadTree,{-100,100,85}),
+    false = intersects_circle(QuadTree,{100,-100,85}),
+    false = intersects_circle(QuadTree,{-100,-100,85}),
 
-    false = intersects(QuadTree,{100,0,1}),
-    false = intersects(QuadTree,{0,100,1}),
-    false = intersects(QuadTree,{-100,0,1}),
-    false = intersects(QuadTree,{0,-100,1}),
+    false = intersects_circle(QuadTree,{100,0,1}),
+    false = intersects_circle(QuadTree,{0,100,1}),
+    false = intersects_circle(QuadTree,{-100,0,1}),
+    false = intersects_circle(QuadTree,{0,-100,1}),
 
 
-    true = intersects(QuadTree,{0,0,10}),
+    true = intersects_circle(QuadTree,{0,0,10}),
 
-    true = intersects(QuadTree,{25,0,10}),
-    true = intersects(QuadTree,{-25,0,10}),
-    true = intersects(QuadTree,{0,25,10}),
-    true = intersects(QuadTree,{0,-25,10}),
+    true = intersects_circle(QuadTree,{25,0,10}),
+    true = intersects_circle(QuadTree,{-25,0,10}),
+    true = intersects_circle(QuadTree,{0,25,10}),
+    true = intersects_circle(QuadTree,{0,-25,10}),
 
-    true = intersects(QuadTree,{25,25,10}),
-    true = intersects(QuadTree,{-25,25,10}),
-    true = intersects(QuadTree,{25,-25,10}),
-    true = intersects(QuadTree,{-25,-25,10}),
+    true = intersects_circle(QuadTree,{25,25,10}),
+    true = intersects_circle(QuadTree,{-25,25,10}),
+    true = intersects_circle(QuadTree,{25,-25,10}),
+    true = intersects_circle(QuadTree,{-25,-25,10}),
+
+    true = intersects_node(
+             #node{x=0,y=0,size=10},
+             #node{x=19,y=19,size=10}),
 
     Tree = lists:foldl(
         fun(Circle,Node)
@@ -75,16 +79,15 @@ test() ->
         end,
         QuadTree,
         ListOfCircles),
-    walk_tree(Tree),
 
-    walk_tree(
-      insert_circle(QuadTree,{3,-4,2})),
-
-    ok.
+%%%     walk_tree(Tree),
+    MyNode = find_node(Tree,{9,9}),
+    find_parent(Tree,MyNode),
+    neighbours(Tree,MyNode).
 
 
 insert_circle(Node,Circle) ->
-    I = intersects(Node,Circle),
+    I = intersects_circle(Node,Circle),
     if
         I ->
 %%            ?LOG({"insert_circle: intersection ",Node,Circle}),
@@ -107,7 +110,8 @@ insert_circle(Node,Circle) ->
                                          fun(ChildNode) ->
                                                  insert_circle(ChildNode,Circle)
                                          end,
-                                         new_children(Node))};
+                                         new_children(Node)),
+                              status=parent};
                         true ->
 %%                            ?LOG("insert_circle: min size, set status obstacle"),
                             Node#node{status=obstacle}
@@ -153,14 +157,102 @@ walk_tree(Node) ->
             io:format("~p ~p~n", [string:copies(" ",50-trunc(2*Node#node.size)),{Node#node.x,Node#node.y}])
     end.
 
+find_node(Node,{X,Y}) ->
+    io:format("find_node: ~p ~p~n", [string:copies(" ",50-trunc(2*Node#node.size)),{Node#node.x,Node#node.y}]),
+    if
+        is_list(Node#node.children) ->
+            ?LOG({"find_node: children"}),
+            [MyChild] = lists:filter(
+              fun(ChildNode) ->
+                      within_node(ChildNode,{X,Y})
+              end,
+              Node#node.children),
+            find_node(MyChild,{X,Y});
+        true -> 
+            ?LOG({"find_node: leafnode"}),
+            Node
+    end.
+
+find_parent(Node,Leaf) ->
+    io:format("find_parent: ~p ~p~n", [string:copies(" ",50-trunc(2*Node#node.size)),{Node#node.x,Node#node.y}]),
+    if
+        is_list(Node#node.children) ->
+            [MyChild] = lists:filter(
+              fun(ChildNode) ->
+                      within_node(ChildNode,{Leaf#node.x,Leaf#node.y})
+              end,
+              Node#node.children),
+            if
+                is_list(MyChild#node.children) ->
+                    find_parent(MyChild,Leaf);
+                true -> Node
+            end;
+        true -> throw(empty_tree)
+    end.
 
 
+within_node(Node,{X,Y}) ->
+    (((Node#node.x-Node#node.size) < X)
+     and (X =< (Node#node.x+Node#node.size)))
+        and
+          (((Node#node.y-Node#node.size) < Y)
+           and (Y =< (Node#node.y+Node#node.size))).
+
+within_node2(Node,Point) ->
+    within(Point,corners({Node#node.x,Node#node.y,Node#node.size})).
+
+within({X,Y},[{X1,Y1},{X2,Y2},_,_]) ->
+    ((X1 =< X) and (X =< X2))
+        and
+          ((Y1 =< Y) and (Y =< Y2)).
+
+corners({X,Y,Size}) ->
+    [{X-Size,Y-Size},
+     {X+Size,Y+Size},
+     {X+Size,Y-Size},
+     {X-Size,Y+Size}].
+
+intersects_node(Node1,Node2) ->
+    C1 = corners({Node1#node.x,Node1#node.y,Node1#node.size}),
+    C2 = corners({Node2#node.x,Node2#node.y,Node2#node.size}),
+    lists:any(
+      fun(Point) -> within(Point,C1) end,
+      C2) or
+        lists:any(
+          fun(Point) -> within(Point,C2) end,
+          C1).
+
+neighbours(Node,Leaf) ->
+    neighbours_rec(Node,
+                   Leaf#node{ size = Leaf#node.size + ?MINSIZE/2 }).
+
+neighbours_rec(Node,Leaf) ->
+    io:format("neighbours: ~p ~p~n", [string:copies(" ",50-trunc(2*Node#node.size)),{Node#node.x,Node#node.y}]),
+    I = intersects_node(Node,Leaf),
+    if
+        I ->
+            ?LOG({"neighbours: intersects"}),
+            if
+                is_list(Node#node.children) ->
+                    ?LOG({"neighbours: children"}),
+                    lists:flatmap(
+                      fun(ChildNode) ->
+                              neighbours_rec(ChildNode,Leaf)
+                      end,
+                      lists:filter(
+                        fun(ChildNode) ->
+                                intersects_node(ChildNode,Leaf)
+                        end,
+                        Node#node.children));
+                true ->
+                    ?LOG({"neighbours: leafnode"}),
+                    [Node]
+            end;
+        true -> []
+    end.
 
 
-
-
-
-intersects(Node,{X,Y,R}) ->
+intersects_circle(Node,{X,Y,R}) ->
     NodeXmax = Node#node.x+Node#node.size,
     NodeXmin = Node#node.x-Node#node.size,
     NodeYmax = Node#node.y+Node#node.size,
@@ -220,7 +312,6 @@ intersects(Node,{X,Y,R}) ->
             end;
         true -> false
     end.
-
 
 sqr(X) ->
     X*X.
